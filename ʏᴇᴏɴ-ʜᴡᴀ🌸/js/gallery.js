e5127f0b96fb95546cc44e793a829e0772f330b1
dreamqavk <leonovvlad098@gmail.com>
2026-03-24 15:24:59 +0300

    Additions: Added a new category: Coaching Changes: Minor text adjustments

--- ʏᴇᴏɴ-ʜᴡᴀ🌸/js/gallery.js
+++ ʏᴇᴏɴ-ʜᴡᴀ🌸/js/gallery.js
@@ -1,7 +1,7 @@
 const gamesData = [
     { image: "https://fons.grizly.club/uploads/posts/2025-06/26/17509434006283.jpg", name: "Minecraft" },
     { image: "https://www.kinonews.ru/insimgs/2020/poster/poster94404_1.jpg", name: "Valorant" },
-    { image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQek_z7yyC4fLjKLPqXccvXYe1YpvmLb0O3uA&s", name: "CS 2" },
+    { image: "https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/730/capsule_616x353.jpg?t=1749053861", name: "CS 2" },
     { image: "https://fons.grizly.club/uploads/posts/2025-06/26/1750930811559.jpg", name: "Dota 2" },
     { image: "https://i.playground.ru/i/pix/3117450/image.jpg", name: "Genshin Impact" },
     { image: "https://cdn1.epicgames.com/offer/24b9b5e323bc40eea252a10cdd3b2f10/EGS_LeagueofLegends_RiotGames_S2_1200x1600-112729f9da450fe377e11d40029c4831", name: "League of Legends" },
